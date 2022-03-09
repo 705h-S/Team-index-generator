ec8400ca211f19856061dff076a080b463f8b0eb
705H-S <93358442+705h-S@users.noreply.github.com>
2022-03-08 21:04:34 -0800

    generate engineer

--- src/Template.js
+++ src/Template.js
@@ -74,4 +74,22 @@ function generateManagerCard (manager) {
         </div>
       </div>
     `;
-  }
+  };
+  // Engineer
+  function generateEngineerCard (engineer) {
+    return `
+      <div class="card mx-4 mb-4" id="engineer-card">
+      <div class="card-header text-center card-title team text-white">
+          <h3>${engineer.name}</h3>
+          <h4><i class="fas fa-glasses mr-2"></i>Engineer</h4>
+        </div>
+        <div class="card-body">
+          <ul class="list-group">
+            <li class="list-group-item">ID: ${engineer.id}</li>
+            <li class="list-group-item">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></li>
+            <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.github}">${engineer.github}</a></li>
+          </ul>
+        </div>
+      </div>
+    `;
+  };
